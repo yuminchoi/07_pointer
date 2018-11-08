3794df052aa0aee63f2bdd27ef665b04e5ca840b
yuminchoi <chldbals219@naver.com>
2018-11-08 16:02:24 +0900

    third sub.

--- main.c
+++ main.c
@@ -4,23 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x=10;
-	int y=20;
+	int grade[5]={10, 20, 30, 40, 50};
+	int average=0;
 	int *ptr;
 	
 	
-	ptr=&x;
-	printf("x : %i\n", *ptr);
+	int i;
 	
 	
-	ptr=&y;
-	printf("y : %i\n", *ptr);
+	ptr=grade;
 	
 	
-	ptr=&x;
-	*ptr=20;
-	printf("x:%i\n", x);
+	for (i=0;i<5;i++)
+	{
+		average+=ptr[i]; //*(ptr+i); //ptr[i]  
+	}
 	
+	
+	printf("average:%i\n", average/5);	
 	
 	return 0;
 }
